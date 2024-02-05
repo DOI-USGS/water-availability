7239c40e2ee0a11c5769a220526533cc0a57cecc
Cee Nell <cnell@usgs.gov>
2024-02-05 12:31:24 -0800

    blank site

---
+++ src/views/VisualizationView.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+   
+  </section>
+</template>
+
+<script setup>
+  //import Wavy from '../components/WavyWaves.vue';
+</script>
+
+<style scoped>
+.section-header {
+  color: var(--dodger-blue);
+}
+
+
+</style>
