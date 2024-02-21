c65ffe86a19f478ecc2d495cad3df212c71e42c0
Althea A Archer <aaarcher@usgs.gov>
2024-02-21 11:38:49 -0600

    basic build of svgs

---
+++ 2_process/src/process_region_shp.R
@@ -0,0 +1,24 @@
+#' @title Prepare the shapefiles into sf for creating svgs/maps
+#' @param in_shp The shapefile pathway that is used to build the sf
+#' 
+regions_to_sf <- function(in_shp){
+  hr <- st_read(in_shp)
+  
+  # Simplify sfs and remove gaps (e.g., Columbia River)
+  hr_simplified <- hr |> 
+    # Simplify polyons
+    rmapshaper::ms_simplify(keep = 0.01) |> 
+    # Remove gaps
+    sf::st_buffer(1800) |> 
+    sf::st_difference() |>
+    # Correct invalid topology incorrect geometry types
+    sf::st_make_valid() |> 
+    sf::st_cast("MULTIPOLYGON") |> 
+    nngeo::st_remove_holes() |> 
+    # Transform spatial projection
+    st_transform(crs = "EPSG: 5070") |>
+    select(-Region_nam, -AggRegion, -AggRegion_)
+    
+  return(hr_simplified)
+  
+}
