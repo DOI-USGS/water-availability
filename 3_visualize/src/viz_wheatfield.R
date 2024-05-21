c04a85a17bc02d728c06c082c071e7b13bf26037
Althea A Archer <aaarcher@usgs.gov>
2024-05-21 08:54:11 -0500

    comment out plot code for testing pipeline

--- 3_visualize/src/viz_wheatfield.R
+++ 3_visualize/src/viz_wheatfield.R
@@ -11,19 +11,19 @@ plot_wheatfield <- function(data_in,
       sw_pct = sprintf("%s_sw_pct", use_type)) |>
     mutate(shifted_pct = sw_pct - 0.5) 
     
-  max_vector_length <- 300000
-  max_wu_mgd <- max(temp$total)
-  
-  plot_data_centroids <- temp |>
-    #sf::st_make_valid() |>
-    sf::st_centroid() |>
-    mutate(
-      angle = shifted_pct * (45/0.5) * pi / 180,
-      hyp = total*(max_vector_length/max_wu_mgd),
-      x_dif = cos(angle)*hyp,
-      y_dif = sin(angle)*hyp,
-      x = sf::st_coordinates(.)[,1],
-      y = sf::st_coordinates(.)[,2],
-      x_end = x + x_dif,
-      y_end = y + y_dif)
+  # max_vector_length <- 300000
+  # max_wu_mgd <- max(temp$total)
+  # 
+  # plot_data_centroids <- temp |>
+  #   #sf::st_make_valid() |>
+  #   sf::st_centroid() |>
+  #   mutate(
+  #     angle = shifted_pct * (45/0.5) * pi / 180,
+  #     hyp = total*(max_vector_length/max_wu_mgd),
+  #     x_dif = cos(angle)*hyp,
+  #     y_dif = sin(angle)*hyp,
+  #     x = sf::st_coordinates(.)[,1],
+  #     y = sf::st_coordinates(.)[,2],
+  #     x_end = x + x_dif,
+  #     y_end = y + y_dif)
 }
