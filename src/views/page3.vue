2448eafd8124c9522e3c3192028a23312e070cd4
Althea A Archer <aaarcher@usgs.gov>
2024-05-23 13:21:28 -0500

    add k03 image from idea blitz

--- src/views/page3.vue
+++ src/views/page3.vue
@@ -2,7 +2,9 @@
     <section class="main-container">
         <KeyMessages></KeyMessages>
         <div class="viz-container">
-            Interactive viz goes here
+          <img class="viz-placeholder"
+            src="@/assets/images/k03_sui_popn.png"
+          >
         </div>
         <PageCarousel></PageCarousel>
     </section>
