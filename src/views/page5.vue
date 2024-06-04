2191b44a3cb98a3a405317880e1d006c9a263a45
Althea A Archer <aaarcher@usgs.gov>
2024-06-03 22:18:41 -0500

    placeholder for k05

--- src/views/page5.vue
+++ src/views/page5.vue
@@ -1,22 +1,45 @@
 <template>
     <section class="main-container">
         <KeyMessages></KeyMessages>
+            <button id="image-toggle" class="toggle-button">Click for Total Phosphorus</button>
         <div class="viz-container">
-            Interactive viz goes here
-        </div>
+                <img 
+                    id="first-image" 
+                    class="viz-placeholder" 
+                    :src="photo1" 
+                    alt="xxx"
+                >
+         </div>
         <PageCarousel></PageCarousel>
     </section>
 </template>
 
-<script>
+<script setup>
+import { onMounted } from 'vue';
 import PageCarousel from '../components/PageCarousel.vue';
 import KeyMessages from '../components/KeyMessages.vue';
-export default {
-    components: {
-        PageCarousel,
-        KeyMessages
+import photo1 from "@/assets/images/k05_wq_tn_source.png";
+import photo2 from "@/assets/images/k05_wq_tp_source.png";
+
+onMounted(() => {
+
+    let firstImg = document.getElementById("first-image");
+    let imageToggle = document.getElementById("image-toggle");
+
+
+    function toggleImg() {
+        if(firstImg.getAttribute('src') === photo1) {
+            firstImg.setAttribute('src', photo2);
+            imageToggle.textContent = "Click for Total Nitrogen";
+        }
+        else {
+            firstImg.setAttribute('src', photo1);
+            imageToggle.textContent = "Click for Total Phosphorus";
+        }
     }
-}
+
+    imageToggle.addEventListener("click", toggleImg)
+}); 
 </script>
 
 <style scoped>
