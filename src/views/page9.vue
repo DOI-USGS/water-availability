0ce5b56e3b4ea8468393555e79bb22b7ff16f818
Althea A Archer <aaarcher@usgs.gov>
2024-06-03 21:49:10 -0500

    toggle names k09

--- src/views/page9.vue
+++ src/views/page9.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="main-container">
         <KeyMessages></KeyMessages>
-        <button id="image-toggle" class="toggle-button">Toggle Image</button>
+        <button id="image-toggle" class="toggle-button">Add lag</button>
         <div class="viz-container">
                 <img 
                     id="first-image" 
@@ -32,9 +32,11 @@ let imageToggle = document.getElementById("image-toggle");
 function toggleImg() {
 if(firstImg.getAttribute('src') === photo1) {
     firstImg.setAttribute('src', photo2);
+    imageToggle.textContent = "Remove lag";
 }
 else {
     firstImg.setAttribute('src', photo1);
+    imageToggle.textContent = "Add lag"
 }
 }
 
