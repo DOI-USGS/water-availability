ea8ed17ae56718e9700d0e45738997e1f940da11
Althea A Archer <aaarcher@usgs.gov>
2024-08-21 16:50:41 -0500

    organize content, kf3,4

--- src/views/keyFinding03SUISVI.vue
+++ src/views/keyFinding03SUISVI.vue
@@ -1,17 +1,21 @@
 <template>
     <section class="main-container">
         <KeyMessages></KeyMessages>
-        <button id="image-toggle" class="toggle-button">View Dry Areas Only</button>
-
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat.</p>
-
-        <div class="viz-container">
-            <img 
-                    id="first-image" 
-                    class="viz-placeholder" 
-                    :src="photoAll" 
-                    alt="xxx"
-                >
+        <div class="content-container">
+            <div class="text-container">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat.</p>
+            </div>
+            <div class="button-container">
+                <button id="image-toggle" class="toggle-button">View Dry Areas Only</button>
+            </div>
+            <div class="viz-container">
+                <img 
+                        id="first-image" 
+                        class="viz-placeholder" 
+                        :src="photoAll" 
+                        alt="xxx"
+                    >
+        </div>
         </div>
         <PageCarousel></PageCarousel>
     </section>
