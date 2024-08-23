3e3648493094029e03bb1dcd75906253a3f0b173
Althea A Archer <aaarcher@usgs.gov>
2024-08-23 11:01:20 -0500

    reorganize content, kf6-10

--- src/views/keyFinding10FutureWater.vue
+++ src/views/keyFinding10FutureWater.vue
@@ -1,10 +1,13 @@
 <template>
     <section class="main-container">
         <KeyMessages></KeyMessages>
-        <div class="viz-container">
-            <img class="viz-placeholder"
-            src="@/assets/images/k02_future.png"
-          >
+        <div class="content-container">
+            <div class="text-container">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat.</p>
+            </div>
+            <div class="viz-container">
+                <img class="viz-placeholder" src="@/assets/images/k02_future.png">
+            </div>
         </div>
         <PageCarousel></PageCarousel>
     </section>
