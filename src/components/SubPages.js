b5a259fdf0d4a2c1ba5db0b39cab19f31064ed6b
Althea A Archer <aaarcher@usgs.gov>
2024-10-16 15:32:12 -0500

    draft kf 4

--- src/components/SubPages.js
+++ src/components/SubPages.js
@@ -3,32 +3,79 @@ export default {
         {
             page: 1,
             route: '/keyFinding01Budget',
-            keyMessage: 'A quarter of our daily water budget leaves the U.S. through streamflow to Canada, the Atlantic and Pacific Oceans, or the Gulf of Mexico.'
+            shortTitle: 'The U.S. has enough water, but not regionally',
+            keyMessage: 'At the national scale, the natural water supply greatly exceeds our demand for water. However, this pattern is reversed in some regions.'
         },
         {
             page: 2,
             route: '/keyFinding02SUIPopn',
+            shortTitle: 'Water stress affects 30 million people',
             keyMessage: 'More than 30 million people live in areas of high/severe water stress.'
         },
         {
             page: 3,
             route: '/keyFinding03SUISVI',
+            shortTitle: 'The socially vulnerable are disproportionately affected by water stress',
             keyMessage: 'A high proportion of the population living in areas with water availability limitations are considered to be socially vulnerable.'
         },
         {
             page: 4,
             route: '/keyFinding04Drought',
-            keyMessage: 'Periods of low precipitation can have cascading effects throughout the water cycle, expanding areas of water stress and exacerbating water quality issues.' 
+            shortTitle: 'Disruptions to the water cycle can cause water stress',
+            keyMessage: 'Periods of low precipitation can have cascading effects throughout the water cycle, expanding areas of water stress and exacerbating water avaiability issues.' ,
+            references: [
+                {
+                    reference: "Reference 1.",
+                    num: "1",
+                    authors: "Bell, J.E., Leeper, R.D., Palecki, M.A., Coopersmith, E., Wilson, T., Bilotta, R., and Embler, S.",
+                    year: "2015",
+                    title: "Evaluation of the 2012 Drought with a Newly Established National Soil Monitoring Network",
+                    journal: true,
+                    journal_name: "Vadose Zone Journal",
+                    journal_issue: "14",
+                    data_release: false,
+                    link: "https://doi.org/10.2136/vzj2015.02.0023",
+                    doi: "10.2136/vzj2015.02.0023"
+                  },
+                {
+                    reference: "Reference 2.",
+                    num: "2",
+                    authors: "Hoerling, M., Eischeid, J., Kumar, A., Leung, R., Mariotti, A., Mo, K., Schubert, S., and Seager, R.",
+                    year: "2014",
+                    title: "Causes and Predictability of the 2012 Great Plains Drought",
+                    journal: true,
+                    journal_name: "Bulletin of the American Meteorological Society",
+                    journal_issue: "95",
+                    data_release: false,
+                    link: "https://doi.org/10.1175/BAMS-D-13-00055.1",
+                    doi: "10.1175/BAMS-D-13-00055.1"
+                  },
+                {
+                    reference: "Reference 3.",
+                    num: "3",
+                    authors: "Legates, D.R., Mahmood, R., Levia, D.F., DeLiberty, T.L., Quiring, S.M., Houser, C., and Nelson, F.E.",
+                    year: "2011",
+                    title: "Soil moisture: A central and unifying theme in physical geography",
+                    journal: true,
+                    journal_name: "Progress in Physical Geography: Earth and Environment",
+                    journal_issue: "35",
+                    data_release: false,
+                    link: "https://doi.org/10.1177/0309133310386514",
+                    doi: "10.1177/0309133310386514"
+                  }
+            ]
         },
         {
             page: 5,
             route: '/keyFinding05WQSource',
-            keyMessage: 'Water quality is affected by natural and human sources, including springs and streams, wastewater, land use activities, and air pollution.'
+            shortTitle: 'Sources of water contamination',
+            keyMessage: 'Water quality is affected by natural and human source.'
         },
         {
             page: 6,
             route: '/keyFinding06WQThreats',
-            keyMessage: 'Threats to water quality depend on what the water is used for (e.g., drinking water versus recreation) and where the water is coming from (e.g., surface water, groundwater).',
+            shortTitle: 'Threats to drinking water',
+            keyMessage: 'Threats to water quality depend on what the water is used for and where the water is coming from.',
             references: [
                 {
                   reference: "Reference 1.",
@@ -113,21 +160,25 @@ export default {
         {
             page: 7,
             route: '/keyFinding07WaterUse',
-            keyMessage: 'About 225,000 million gallons of freshwater get used per day for crop irrigation, public supply, and thermoelectric power generation in the lower 48 states.'
+            shortTitle: 'Top water use categories',
+            keyMessage: 'About 225,000 million gallons of freshwater get used per day for crop irrigation, public supply, and thermoelectric power.'
         },
         {
             page: 8,
             route: '/keyFinding08WaterUseRegional',
-            keyMessage: 'How we use water varies across the country, including the source of water (groundwater versus surface water) and the type of use (e.g., crop irrigation, public supply, or thermoelectric power generation).'
+            shortTitle: 'Water use varies regionally',
+            keyMessage: 'How we use water varies across the country, including the source of water and the type of use.'
         },
         {
             page: 9,
             route: '/keyFinding09WaterUseConsumption',
+            shortTitle: 'Not all the water we use is returned',
             keyMessage: 'Not all the water withdrawn for human water use returns to the local environment.'
         },
         {
             page: 10,
             route: '/keyFinding10FutureWater',
+            shortTitle: 'Water availability will be affected by climate change',
             keyMessage: 'The top future threats to water availability in the U.S. vary regionally and include increases in drought, flooding, and heavy precipitation along with declines in snow and ice.'
         }
     ]
