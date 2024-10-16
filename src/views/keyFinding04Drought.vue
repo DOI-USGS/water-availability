591e4b31d6bc33669d5f815ec289fbb060390055
Althea A Archer <aaarcher@usgs.gov>
2024-10-16 15:39:21 -0500

    relink images

--- src/views/keyFinding04Drought.vue
+++ src/views/keyFinding04Drought.vue
@@ -7,7 +7,7 @@
                 <p>Water evaporates from the ocean and land into the atmosphere where its movement is driven by wind. In the atmosphere, it condenses to form precipitation which falls to the Earth's surface in the form of rain or snow. Precipitation that falls on land has many fates. It can be held temporally as snow or soil moisture, stored in lakes or reservoirs, return to the atmosphere through evaporation or transpiration (evapotranspiration), run off into streams and rivers eventually discharging into the ocean, or consumed by plants and animals. A small fraction of the precipitation that falls on land percolates more deeply to recharge groundwater. In the United States, a quarter of our daily water budget leaves through streamflow to Canada, the Atlantic and Pacific Oceans, or the Gulf of Mexico.</p>
                 </div>
                 <div class="viz-container">
-                    <img class="viz-placeholder" src="@/assets/images/k01_sankey.png">
+                    <img class="viz-placeholder" src="../../public/images/k04_watercycle.png">
                 </div>
                 <div class="text-container">
                 <h2>Disruptions to the water cycle can cause water stress</h2>
@@ -16,8 +16,8 @@
                 </p>
             </div>
             <div class="viz-container">
-                <img class="viz-double-portrait" src="../../public/images/A.png">
-                <img class="viz-double-portrait" src="../../public/images/B.png">
+                <img class="viz-double-portrait" src="../../public/images/k04_Prec.png">
+                <img class="viz-double-portrait" src="../../public/images/k04_noPrec.png">
             </div>
             <div class="text-container">
                 <p>Precipitation and snowmelt improve quickflow, help fill streams, and can recharge the water available to plants in the ground. Soil moisture provides water for plants in the root zone and recycling water back to the atmosphere through plant transpiration and evaporation from bare soils (Legates et al., 2011). During summer months in the Souris-Red-Rainy, Texas, Southern High Plains, and Southwest Desert hydrologic regions soil moisture volumes are very low, indicating that the precipitation that falls during these times is directly lost to streamflow and/or plants and relatively little is partitioned to groundwater storage. 
