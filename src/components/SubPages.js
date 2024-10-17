2a7b9d8baf87363b38810866eb28f28e59932f02
Althea A Archer <aaarcher@usgs.gov>
2024-10-17 15:45:10 -0500

    filters one

--- src/components/SubPages.js
+++ src/components/SubPages.js
@@ -30,84 +30,12 @@ export default {
             route: '/keyFinding06WQThreats',
             keyMessage: 'Threats to water quality depend on what the water is used for (e.g., drinking water versus recreation) and where the water is coming from (e.g., surface water, groundwater).',
             references: [
-                {
-                  reference: "Reference 1.",
-                  num: "1",
-                  authors: "Cañedo-Argüelles, M., Kefford, B.J., Piscart, C., Prat, N., Schäfer, R.B., and Schulz, C.-J.",
-                  year: "2013",
-                  title: "Salinisation of rivers: An urgent ecological issue",
-                  journal: true,
-                  journal_name: "Environmental Pollution",
-                  journal_issue: "173",
-                  data_release: false,
-                  link: "https://doi.org/10.1016/j.envpol.2012.10.011",
-                  doi: "10.1016/j.envpol.2012.10.011"
-                },
-                {
-                  reference: "Reference 2.",
-                  num: "2",
-                  authors: "Kaushal, S.S., Likens, G.E., Pace, M.L., Utz, R.M., Haq, S., Gorman, J., and Grese, M.",
-                  year: "2018",
-                  title: "Freshwater salinization syndrome on a continental scale",
-                  link: "https://doi.org/10.1007/s10533-021-00784-w",
-                  journal: true,
-                  journal_name: "Proceedings of the National Academy of Sciences",
-                  journal_issue: "115",
-                  data_release: false,
-                  doi: "10.1007/s10533-021-00784-w"
-                },
-                {
-                  reference: "Reference 3.",
-                  num: "3",
-                  authors: "Ngoubeyou, P.S.K., Wolkersdorfer, C., Ndibewu, P.P., and Augustyn, W.",
-                  year: "2022",
-                  title: "Toxicity of polychlorinated biphenyls in aquatic environments – A review",
-                  link: "https://doi.org/10.1016/j.aquatox.2022.106284",
-                  journal: true,
-                  journal_name: "Aquatic Toxicology",
-                  journal_issue: "251",
-                  data_release: false,
-                  doi: "10.1016/j.aquatox.2022.106284"
-                },
-                {
-                  reference: "Reference 4.",
-                  num: "4",
-                  authors: "United States Environmental Protection Agency (US EPA)",
-                  year: "2023",
-                  title: "Total Maximum Daily Load (TMDL) Tracking and Implementation System (ATTAINS)",
-                  journal: false,
-                  journal_name: "",
-                  journal_issue: "",
-                  data_release: false,
-                  link: "https://www.epa.gov/waterdata/attains",
-                  doi: null
-                },
-                {
-                  reference: "Reference 5.",
-                  num: "5",
-                  authors: "Vareda, J.P., Valente, A.J.M., and Durães, L.",
-                  year: "2019",
-                  title: "Assessment of heavy metal pollution from anthropogenic activities and remediation strategies: A review:",
-                  journal: true,
-                  journal_name: "Journal of Environmental Management",
-                  journal_issue: "246",
-                  data_release: false,
-                  link: "https://doi.org/10.1016/j.jenvman.2019.05.126",
-                  doi: "10.1016/j.jenvman.2019.05.126"
-                },
-                {
-                  reference: "Reference 6.",
-                  num: "6",
-                  authors: "Wentz, D.A., Brigham, M.E., Chasar, L.C., Lutz, M.A., and Krabbenhoft, D.P.",
-                  year: "2014",
-                  title: "Mercury in the nation's streams - Levels, trends and implications [Report]",
-                  link: "https://doi.org/10.3133/cir1395",
-                  journal: false,
-                  journal_name: "",
-                  journal_issue: "",
-                  data_release: false,
-                  doi: "10.3133/cir1395"
-                }
+                "Canedo2013",
+                "Kaushal2018",
+                "Ngoubeyou2022",
+                "EPA2023",
+                "Vareda2019",
+                "Wentz2014"
             ]
         },
         {
