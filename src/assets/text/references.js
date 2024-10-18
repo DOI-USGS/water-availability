056525571ba2e7c3dcc540a0bd78fd3a6edfc900
Althea A Archer <aaarcher@usgs.gov>
2024-10-18 07:21:35 -0500

    social vulnerability text from Mandie

--- src/assets/text/references.js
+++ src/assets/text/references.js
@@ -86,6 +86,54 @@ export default {
         data_release: false,
         link: "https://doi.org/10.1016/j.jenvman.2019.05.126",
         doi: "10.1016/j.jenvman.2019.05.126"
-      }
+      },
+      {
+        refID: "Bell2015",
+        authors: "Bell, J.E., Leeper, R.D., Palecki, M.A., Coopersmith, E., Wilson, T., Bilotta, R., and Embler, S.",
+        year: "2015",
+        title: "Evaluation of the 2012 Drought with a Newly Established National Soil Monitoring Network",
+        journal: true,
+        journal_name: "Vadose Zone Journal",
+        journal_issue: "14",
+        data_release: false,
+        link: "https://doi.org/10.2136/vzj2015.02.0023",
+        doi: "10.2136/vzj2015.02.0023"
+      },
+    {
+        refID: "Hoerling2014",
+        authors: "Hoerling, M., Eischeid, J., Kumar, A., Leung, R., Mariotti, A., Mo, K., Schubert, S., and Seager, R.",
+        year: "2014",
+        title: "Causes and Predictability of the 2012 Great Plains Drought",
+        journal: true,
+        journal_name: "Bulletin of the American Meteorological Society",
+        journal_issue: "95",
+        data_release: false,
+        link: "https://doi.org/10.1175/BAMS-D-13-00055.1",
+        doi: "10.1175/BAMS-D-13-00055.1"
+      },
+    {
+        refID: "Legates2011", 
+        authors: "Legates, D.R., Mahmood, R., Levia, D.F., DeLiberty, T.L., Quiring, S.M., Houser, C., and Nelson, F.E.",
+        year: "2011",
+        title: "Soil moisture: A central and unifying theme in physical geography",
+        journal: true,
+        journal_name: "Progress in Physical Geography: Earth and Environment",
+        journal_issue: "35",
+        data_release: false,
+        link: "https://doi.org/10.1177/0309133310386514",
+        doi: "10.1177/0309133310386514"
+      },
+      {
+          refID: "Azadpour2024", 
+          authors: "Azadpour, E., Clarke A., Carr, A., Drakes, O., Restrepo-Osario, D., Alanis, Y.A., Gonzales, P.R., and Nell, C.",
+          year: "2024",
+          title: "Unequal access to water: how societal factors shape vulnerability to water insecurity, U.S. [USGS Website]",
+          journal: false,
+          journal_name: "",
+          journal_issue: "",
+          data_release: false,
+          link: "https://labs.waterdata.usgs.gov/visualizations/vulnerability-indicators/index.html#/en",
+          doi: null
+        }
     ]
 }
--- src/components/SubPages.js
+++ src/components/SubPages.js
@@ -16,7 +16,8 @@ export default {
             page: 3,
             route: '/keyFinding03SUISVI',
             shortTitle: 'Social vulnerability',
-            keyMessage: 'A high proportion of the population living in areas with water availability limitations are considered to be socially vulnerable.'
+            keyMessage: 'A high proportion of the population living in areas with water availability limitations are considered to be socially vulnerable.',
+            references: [ "Azadpour2024"]
         },
         {
             page: 4,
@@ -24,45 +25,9 @@ export default {
             shortTitle: 'Not enough rain and snow',
             keyMessage: 'Periods of low precipitation can have cascading effects throughout the water cycle, expanding areas of water stress and exacerbating water avaiability issues.' ,
             references: [
-                {
-                    reference: "Reference 1.",
-                    num: "1",
-                    authors: "Bell, J.E., Leeper, R.D., Palecki, M.A., Coopersmith, E., Wilson, T., Bilotta, R., and Embler, S.",
-                    year: "2015",
-                    title: "Evaluation of the 2012 Drought with a Newly Established National Soil Monitoring Network",
-                    journal: true,
-                    journal_name: "Vadose Zone Journal",
-                    journal_issue: "14",
-                    data_release: false,
-                    link: "https://doi.org/10.2136/vzj2015.02.0023",
-                    doi: "10.2136/vzj2015.02.0023"
-                  },
-                {
-                    reference: "Reference 2.",
-                    num: "2",
-                    authors: "Hoerling, M., Eischeid, J., Kumar, A., Leung, R., Mariotti, A., Mo, K., Schubert, S., and Seager, R.",
-                    year: "2014",
-                    title: "Causes and Predictability of the 2012 Great Plains Drought",
-                    journal: true,
-                    journal_name: "Bulletin of the American Meteorological Society",
-                    journal_issue: "95",
-                    data_release: false,
-                    link: "https://doi.org/10.1175/BAMS-D-13-00055.1",
-                    doi: "10.1175/BAMS-D-13-00055.1"
-                  },
-                {
-                    reference: "Reference 3.",
-                    num: "3",
-                    authors: "Legates, D.R., Mahmood, R., Levia, D.F., DeLiberty, T.L., Quiring, S.M., Houser, C., and Nelson, F.E.",
-                    year: "2011",
-                    title: "Soil moisture: A central and unifying theme in physical geography",
-                    journal: true,
-                    journal_name: "Progress in Physical Geography: Earth and Environment",
-                    journal_issue: "35",
-                    data_release: false,
-                    link: "https://doi.org/10.1177/0309133310386514",
-                    doi: "10.1177/0309133310386514"
-                  }
+                "Bell2015",
+                "Hoerling2014",
+                "Legates2011"
             ]
         },
         {
