a06b6cd0fe585ab9900372648d2f95e5cf8132bb
Althea A Archer <aaarcher@usgs.gov>
2024-10-18 06:48:32 -0500

    CN + MC short title suggestions

--- src/components/SubPages.js
+++ src/components/SubPages.js
@@ -3,25 +3,25 @@ export default {
         {
             page: 1,
             route: '/keyFinding01Budget',
-            shortTitle: 'The U.S. has enough water, but not regionally',
+            shortTitle: 'Do we have enough water? Yes and no',
             keyMessage: 'At the national scale, the natural water supply greatly exceeds our demand for water. However, this pattern is reversed in some regions.'
         },
         {
             page: 2,
             route: '/keyFinding02SUIPopn',
-            shortTitle: 'Water stress affects 30 million people',
+            shortTitle: 'Living with water stress',
             keyMessage: 'More than 30 million people live in areas of high/severe water stress.'
         },
         {
             page: 3,
             route: '/keyFinding03SUISVI',
-            shortTitle: 'The socially vulnerable are disproportionately affected by water stress',
+            shortTitle: 'Social vulnerability',
             keyMessage: 'A high proportion of the population living in areas with water availability limitations are considered to be socially vulnerable.'
         },
         {
             page: 4,
             route: '/keyFinding04Drought',
-            shortTitle: 'Disruptions to the water cycle can cause water stress',
+            shortTitle: 'Not enough rain and snow',
             keyMessage: 'Periods of low precipitation can have cascading effects throughout the water cycle, expanding areas of water stress and exacerbating water avaiability issues.' ,
             references: [
                 {
@@ -68,7 +68,7 @@ export default {
         {
             page: 5,
             route: '/keyFinding05WQSource',
-            shortTitle: 'Sources of water contamination',
+            shortTitle: 'Sources of contamination',
             keyMessage: 'Water quality is affected by natural and human source.'
         },
         {
@@ -166,19 +166,19 @@ export default {
         {
             page: 8,
             route: '/keyFinding08WaterUseRegional',
-            shortTitle: 'Water use varies regionally',
+            shortTitle: 'Water use by region',
             keyMessage: 'How we use water varies across the country, including the source of water and the type of use.'
         },
         {
             page: 9,
             route: '/keyFinding09WaterUseConsumption',
-            shortTitle: 'Not all the water we use is returned',
+            shortTitle: 'Gone for good',
             keyMessage: 'Not all the water withdrawn for human water use returns to the local environment.'
         },
         {
             page: 10,
             route: '/keyFinding10FutureWater',
-            shortTitle: 'Water availability will be affected by climate change',
+            shortTitle: 'Future threats to water',
             keyMessage: 'The top future threats to water availability in the U.S. vary regionally and include increases in drought, flooding, and heavy precipitation along with declines in snow and ice.'
         }
     ]
