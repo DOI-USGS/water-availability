1ee8bc69e722dc58fe64d212881497e7aa76325c
Althea A Archer <aaarcher@usgs.gov>
2024-10-20 18:19:37 -0500

    merge

--- src/views/keyFinding01Budget.vue
+++ src/views/keyFinding01Budget.vue
@@ -5,15 +5,6 @@
             <div class="text-container">
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
-<<<<<<< HEAD
-            <div class="viz-container">
-                <p>removed on this branch</p>
-            </div>
-            <div class="text-container">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
-=======
->>>>>>> 1185946a1fa88aabfd49a797c5def77aab27b606
         </div>
         <PageCarousel></PageCarousel>
     </section>
