7d5b944ca642f634077e4f1901fbc460e1287494
Althea A Archer <aaarcher@usgs.gov>
2024-10-20 20:55:25 -0500

    ternary plot

--- 3_visualize/src/viz_ternary.R
+++ 3_visualize/src/viz_ternary.R
@@ -15,3 +15,32 @@ ternary_map <- function(in_sf,
   return(png_out)
   
 }
+
+# Create ternary plot and map
+ternary_plot <- function(in_df, width, height,
+                         png_out){
+  
+  p <- ggtern::ggtern(data = in_df, aes(te_prop, ps_prop, ir_prop))
+  
+
+    p <- p +
+      geom_point(aes(color = color), size = 4, alpha = 0.2) 
+  
+  # uses ggtern package
+  p <- p + 
+    geom_point(data = in_df, color = "black", size = 4, alpha = 0.8) +
+    geom_point(data = in_df, color = "white", size = 3.65) +
+    geom_point(data = in_df, aes(color = color), size = 3.65, alpha = 0.8) +
+    scale_color_identity() +
+    ggtern::limit_tern(1.05, 1.05, 1.05) +
+    labs(x = "TE", y = "PS", z = "IR") +
+    theme_void() +
+    theme(plot.background = element_rect(fill = "transparent", color = NA),
+          panel.background = element_rect(fill = "transparent", color = NA))
+  
+  
+  ggsave(png_out, device = "png", bg = "transparent",
+         dpi = 300, units = "in", width = width, height = height)
+  
+  return(png_out)
+}
