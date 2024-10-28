6c43143901717119769115c706ce2d78b5186b2b
Althea A Archer <aaarcher@usgs.gov>
2024-10-28 13:53:24 -0500

    Methods for keyfinding 2

--- src/assets/text/methods.js
+++ src/assets/text/methods.js
@@ -8,7 +8,7 @@ export default {
       },      {
         page: 2,
         route: '/02SUI',
-        description: "How we made this"
+        description: "To calculate the number of people living in different water stress categories, we merged census data with water stress data by watershed (HUC12). Water stress was calculated as annual average water-supply and use index, which expresses the imbalance between surface water-supply and consumptive uses in the conterminous United States for water years 2010 through 2020. <br><br> The extent to which water quantity, surface water quality, groundwater quality, and reduction of natural flows limit water availability were analyzed against quantitative values for a particular use. For the surface-water quantity indicator, water-supply and use index was used. For the surface water-quality indicator, surface water total nitrogen and total phosphorus were compared to the NRSA Good benchmark. For the groundwater-quality indicator, predicted nitrate and arsenic concentrations were compared to the regulatory enforceable drinking water MCLs. For the ecological flow alteration indicator, predicted alteration of different aspects of the flow regime were compared to those aspects of the flow regime that have been shown to affect regional ecological health."
       },      {
         page: 3,
         route: '/03SVI',
