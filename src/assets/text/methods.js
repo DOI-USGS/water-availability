44fa98d68eca5265870964ceacfeb19885608e80
Althea A Archer <aaarcher@usgs.gov>
2024-10-28 13:13:37 -0500

    methods set up

---
+++ src/assets/text/methods.js
@@ -0,0 +1,47 @@
+export default {
+    key: [
+      // Description of methods
+      {
+        page: 1,
+        route: '/01Budget',
+        description: "How we made this"
+      },      {
+        page: 2,
+        route: '/02SUI',
+        description: "How we made this"
+      },      {
+        page: 3,
+        route: '/03SVI',
+        description: "How we made this"
+      },      {
+        page: 4,
+        route: '/04Drought',
+        description: "How we made this"
+      },      {
+        page: 5,
+        route: '/05WQSource',
+        description: "How we made this"
+      },      {
+        page: 6,
+        route: '/06WQThreats',
+        description: "How we made this"
+      },      {
+        page: 7,
+        route: '/07WaterUse',
+        description: "How we made this"
+      },      {
+        page: 8,
+        route: '/08WaterUseRegional',
+        description: "How we made this"
+      },      {
+        page: 9,
+        route: '/09Consumption',
+        description: "How we made this"
+      },      {
+        page: 10,
+        route: '/10Future',
+        description: "How we made this"
+      }
+
+    ]
+}
---
+++ src/components/Methods.vue
@@ -0,0 +1,36 @@
+<template>
+    <!-- HEADING -->
+    <div class="methods-container" id="heading">
+        <h2>Methodology</h2>
+    </div>
+    <div class="methods-container">
+            <p>
+                {{ thisMethod }}
+            </p>
+    </div>
+
+</template>
+  
+<script setup>
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+import SubPages from './SubPages';
+import Methods from './../assets/text/methods.js';
+
+const route = useRoute();
+
+const path = computed(() => route.path)
+
+// filter to this page's key message
+const methodArray = Methods.key.filter(message => message.route === route.path);
+const thisMethod = methodArray[0].description;
+
+
+
+</script>
+  
+<style scoped lang="scss">
+
+  
+</style>
+  
