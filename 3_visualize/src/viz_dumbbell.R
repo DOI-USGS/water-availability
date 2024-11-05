fc29b4737389ea8c129d14fb5dae16442fd65d7c
Kaysa Vaarre-Lamoureux <kvaarre-lamoureux@usgs.gov>
2024-11-05 11:30:34 -0500

    generate dumbbell plots for all WU types

--- 3_visualize/src/viz_dumbbell.R
+++ 3_visualize/src/viz_dumbbell.R
@@ -11,10 +11,29 @@ dumbbell_gw_v_sw <- function(in_sf,
                              height,
                              png_out){
   
-  in_sf <- in_sf |>
-    dplyr::mutate(x_long = sf::st_coordinates(sf::st_centroid(in_sf))[,1]) |> 
-    dplyr::rename(sw_total_wu = sprintf("%s_sw.x", wu_type),
-                  gw_total_wu = sprintf("%s_gw.x", wu_type))  
+ # in_sf <- in_sf |>
+#    dplyr::mutate(x_long = sf::st_coordinates(sf::st_centroid(in_sf))[,1]) |> 
+#    dplyr::rename(sw_total_wu = sprintf("%s_sw.x", wu_type),
+#                  gw_total_wu = sprintf("%s_gw.x", wu_type))  
+  
+  if(wu_type == "all"){
+    proc_in_sf <- in_sf |>
+      dplyr::mutate(x_long = sf::st_coordinates(sf::st_centroid(in_sf))[,1]) |> 
+    #  dplyr::rename(sw_total_ps = "ps_sw.x",
+    #                gw_total_ps = "ps_gw.x",
+    #                sw_total_ir = "ir_sw.x",
+    #                gw_total_ir = "ir_gw.x",
+    #                sw_total_te = "te_sw.x",
+    #                gw_total_te = "te_gw.x")  |> 
+      dplyr::mutate(sw_total_wu = rowSums(dplyr::across(c(ps_sw.x, ir_sw.x, te_sw.x))),
+                    gw_total_wu = rowSums(dplyr::across(c(ps_gw.x, ir_gw.x, te_gw.x))))
+  }
+  else if(wu_type != "all"){
+    proc_in_sf <- in_sf |>
+      dplyr::mutate(x_long = sf::st_coordinates(sf::st_centroid(in_sf))[,1]) |> 
+      dplyr::rename(sw_total_wu = sprintf("%s_sw.x", wu_type),
+                    gw_total_wu = sprintf("%s_gw.x", wu_type))  
+  }
   
 #  if(agg_reg != "CONUS"){
 #    filtered_in_sf <- in_sf |> 
@@ -26,13 +45,13 @@ dumbbell_gw_v_sw <- function(in_sf,
   #in_CONUS <- in_CONUS |>
     #mutate(x_long = st_coordinates(st_centroid(in_CONUS))[,1])
   
-  max_sw <- in_sf |>
+  max_sw <- proc_in_sf |>
     filter(sw_total_wu == max(sw_total_wu)) 
-  max_gw <- in_sf |>
+  max_gw <- proc_in_sf |>
     filter(gw_total_wu == max(gw_total_wu))
   
   
-  plot <- ggplot(in_sf)  + #in_CONUS
+  plot <- ggplot(proc_in_sf)  + #in_CONUS
     geom_linerange(aes(x = x_long,
                        ymax = sw_total_wu,
                        ymin = 0),
@@ -54,13 +73,13 @@ dumbbell_gw_v_sw <- function(in_sf,
   
   if(agg_reg == "CONUS"){
     plot <- plot +
-      geom_linerange(data = in_sf, 
+      geom_linerange(data = proc_in_sf, 
                      aes(x = x_long,
                          ymax = sw_total_wu,
                          ymin = 0),
                      color = "#065867", alpha = 0.6,
                      linewidth = 1.5) +
-      geom_linerange(data = in_sf,
+      geom_linerange(data = proc_in_sf,
                      aes(x = x_long,
                          ymin = gw_total_wu*-1,
                          ymax = 0),
@@ -68,13 +87,13 @@ dumbbell_gw_v_sw <- function(in_sf,
                      linewidth = 1.5) 
   } else if(agg_reg != "CONUS"){
     plot <- plot+
-      geom_linerange(data = subset(in_sf, AggRegion_nam == agg_reg), 
+      geom_linerange(data = subset(proc_in_sf, AggRegion_nam == agg_reg), 
                      aes(x = x_long,
                          ymax = sw_total_wu,
                          ymin = 0),
                      color = "#065867", alpha = 0.6,
                      linewidth = 1.5) +
-      geom_linerange(data = subset(in_sf, AggRegion_nam == agg_reg),
+      geom_linerange(data = subset(proc_in_sf, AggRegion_nam == agg_reg),
                      aes(x = x_long,
                          ymin = gw_total_wu*-1,
                          ymax = 0),
