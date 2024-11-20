a96d34b7d47b9d863b176a8bdf58c9c3461e3f25
Kaysa Vaarre-Lamoureux <kvaarre-lamoureux@usgs.gov>
2024-11-20 11:52:20 -0500

    clean up code

--- 3_visualize/src/viz_availability.R
+++ 3_visualize/src/viz_availability.R
@@ -11,27 +11,13 @@ water_avail_map <- function(in_df, wa_type, color_scheme, png_out, width, height
                "moderate" = "#b17457",
                "high" = "#603f26",
                "severe" = "#181c14")
-   
-  if(wa_type == "wa_sw_wq"){
-    title <- "surface water quality"
-  }else if(wa_type == "wa_sui"){
-    title <- "surface water balance"
-  }else if (wa_type == "wa_gw_wq"){
-    title <- "groundwater quality"
-  }else if(wa_type == "wa_ecoflow"){
-    title <- "ecological flow alteration"
-  }
   
   ggplot(plot_data) +
-    geom_sf(aes(fill = stress), color = color_scheme$svg_col_default) +
+    geom_sf(aes(fill = stress), color = color_scheme$svg_col_default, linewidth = 0.5) +
     scale_fill_manual(values = c(col_pal),
                       breaks = c("very low", "low", "moderate", "high", "severe"))+
-    #ggtitle(title)+
     theme_void()+
     theme(legend.position = "none"
-      #legend.direction = "horizontal",
-          #legend.text = element_blank(),
-          #legend.title = element_blank()
       )
   
   ggsave(filename = png_out, device = "png", bg = "transparent",
