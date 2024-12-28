ceb6765b92b997ae0008183e5805bc356d8322f1
Cee <cnell@usgs.gov>
2024-12-28 17:40:59 -0600

    use new area fxn

--- 2_process/src/process_wq_data.R
+++ 2_process/src/process_wq_data.R
@@ -46,8 +46,8 @@ prep_wq_for_sankey <- function(data_in, unimpair_miles){
     arrange(-riverMiles)
 }
 
-
-summary_wq_by_state <- function(in_sf, nutrient, out_csv){
+summary_wq_by_area <- function(in_sf, nutrient, out_csv, by = c("region", "state")){
+  by <- match.arg(by)
   
   load_column <- sym(ifelse(nutrient == "tn", "tn_load", "tp_load"))
   
@@ -67,86 +67,37 @@ summary_wq_by_state <- function(in_sf, nutrient, out_csv){
     filter(!is.na(!!load_column)) |> 
     mutate(load_level = cut(!!load_column, breaks = breaks, labels = labels))
   
-  # Expand each HUC to its state (some hucs overlap states)
-  expand_states <- category_sf |>
-    select(STATES, HUC12, Region_nam, AggReg_nam, !!load_column, load_level) |>
-    separate_rows(STATES, sep = ",")
+  # determine grouping column
+  group_col <- if (by == "state") "STATES" else "Region_nam"
   
-  # Calculate total number of HUCS per state
-  HUC_per_state <- expand_states |> 
-    sf::st_drop_geometry() |>
-    group_by(STATES) |>
-    mutate(total_hucs = n()) |>
-    ungroup()
-  
-  # Calculate total hucs in each sui category by state and proportion
-  summary_out <- HUC_per_state |>
-    group_by(load_level, STATES, total_hucs) |>
-    summarize(n_cat = n()) |>
-    mutate(prop_cat = n_cat / total_hucs) |>
-    rename(d3_percentage = prop_cat,
-           d3_category = load_level) |>
-    ungroup() |>
-    complete(STATES, d3_category)
-  
-  
-  # save to csv
-  readr::write_csv(summary_out, file = out_csv)
-  
-}
-
-summary_wq_by_region <- function(in_sf, nutrient, out_csv){
-  
-  load_column <- sym(ifelse(nutrient == "tn", "tn_load", "tp_load"))
-  
-  # set load level breaks
-  if(nutrient == "tn") {
-    breaks <- c(100, 500, 1000, 2000, 3000, 6000, 12000, 30000, 120000, Inf)
-    labels <- c("0 - 100", "100 - 500", "500 - 1000", "1000 - 2000", "2000 - 3000", 
-                "3000 - 6000", "6000 - 12000", "12000 - 30000", ">30000")
-  } else {
-    breaks <- c(10, 40, 85, 160, 290, 520, 1000, 2500, 10000, Inf)
-    labels <- c("0 - 10", "10 - 40", "40 - 85", "85 - 160", "160 - 290", 
-                "290 - 520", "520 - 1000", "1000 - 2500", ">2500")
+  if(by == "state") {
+    category_sf <- category_sf |> separate_rows(STATES, sep = ",")
   }
   
-  # categorize load levels
-  category_sf <- in_sf |> 
-    filter(!is.na(!!load_column)) |> 
-    mutate(load_level = cut(!!load_column, breaks = breaks, labels = labels))
-  
-  # summarize by region and category
-  region_summary <- category_sf |>
-    sf::st_drop_geometry() |>
-    rename(load = !!load_column) |>
-    select(HUC12, Region_nam, AggReg_nam, load, load_level, Area_sqkm) |>
-    group_by(Region_nam, AggReg_nam, load_level) |>
+  # calculate summaries
+  summary_data <- category_sf |> 
+    sf::st_drop_geometry() |> 
+    group_by(!!sym(group_col), load_level) |> 
     summarize(category_hucs = n(),
               category_sqkm = sum(Area_sqkm, na.rm = TRUE),
-              total_load = round(sum(load, na.rm = TRUE), 4))
+              total_load = round(sum(!!load_column, na.rm = TRUE), 4)) |> 
+    ungroup()
   
-  # Calculate total number of HUCS per state
-  HUC_per_region <- category_sf |> 
-    sf::st_drop_geometry() |>
-    group_by(Region_nam) |>
+  total_summary <- category_sf |> 
+    sf::st_drop_geometry() |> 
+    group_by(!!sym(group_col)) |> 
     summarize(total_hucs = n(),
-              total_sqkm = sum(Area_sqkm, na.rm = TRUE)) 
+              total_sqkm = sum(Area_sqkm, na.rm = TRUE))
   
-  # Calculate total hucs in each sui category by state and proportion
-  summary_out <- region_summary |>
-    left_join(HUC_per_region, by = "Region_nam") |>
-    mutate(prop_cat = round((category_hucs / total_hucs), 4),
-           prop_sqkm = round((category_sqkm / total_sqkm), 4)) |>
-    rename(d3_percentage = prop_cat,
-           d3_category = load_level) |>
-    ungroup() |>
-    complete(d3_category, Region_nam,
-             fill = list(d3_percentage = 0,
-                         category_hucs = 0,
-                         total_load = 0,
-                         prop_sqkm = 0))
-  
-  
+  # merge summaries and calculate proportions
+  summary_out <- summary_data |> 
+    left_join(total_summary, by = group_col) |> 
+    mutate(prop_sqkm = round((category_sqkm / total_sqkm), 4),
+           d3_percentage = round((category_hucs / total_hucs), 4)) |> 
+    rename(d3_category = load_level) |> 
+    ungroup() |> 
+    complete(!!sym(group_col), d3_category, 
+             fill = list(category_hucs = 0, category_sqkm = 0, total_load = 0, prop_sqkm = 0, d3_percentage = 0))
   
   # save to csv
   readr::write_csv(summary_out, file = out_csv)
