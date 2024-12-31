05e440230a1a9314069fefa1f35e6e5f5067c8c0
Althea A Archer <aaarcher@usgs.gov>
2024-12-30 18:23:51 -0600

    updated colors for maps

--- 3_visualize/src/viz_availability.R
+++ 3_visualize/src/viz_availability.R
@@ -5,12 +5,32 @@ water_avail_map <- function(in_df, wa_type, color_scheme, png_out, width, height
     select(Region_nam, matches(wa_type), geometry) |> 
     rename(stress = 2) |> 
     sf::st_as_sf()
-
-  col_pal <- c("very low" = "#f9d689",
-               "low" = "#e0a75e",
-               "moderate" = "#b17457",
-               "high" = "#603f26",
-               "severe" = "#181c14")
+  
+  if(wa_type == "wa_sw_wq") {
+    col_pal <- c("very low" = "#F0EAF9",
+                 "low" = "#E8CAE1",
+                 "moderate" = "#DCA1C2",
+                 "high" = "#AB5F90",
+                 "severe" = "#6B3858")
+  } else if(wa_type == "wa_sui") {
+    col_pal <- c("very low" = "#E6C398",
+                 "low" = "#E5AA7F",
+                 "moderate" = "#EA906D",
+                 "high" = "#E07560",
+                 "severe" = "#733303")
+  } else if(wa_type == "wa_gw_wq") {
+    col_pal <- c("very low" = "#AB99B8",
+                 "low" = "#7D628E",
+                 "moderate" = "#585380",
+                 "high" = "#403261",
+                 "severe" = "#260C3F")
+  } else if(wa_type == "wa_ecoflow") {
+    col_pal <- c("very low" = "#C6EFB1",
+                 "low" = "#94CB78",
+                 "moderate" = "#7FA35C",
+                 "high" = "#788554",
+                 "severe" = "#545E3B")
+  }
   
   ggplot(plot_data) +
     geom_sf(aes(fill = stress), color = color_scheme$svg_col_default, linewidth = 0.5) +
