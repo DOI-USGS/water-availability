1b42ae72d14f3b90e8fdba7aa4e449d6ea944281
Cee <cnell@usgs.gov>
2025-01-06 00:11:14 -0600

    try to get component to work

--- src/views/keyFinding07WaterUse.vue
+++ src/views/keyFinding07WaterUse.vue
@@ -23,35 +23,11 @@
         <div id="barplot-container"></div>
       </div>
       <div class="caption-container">
-        <div class="caption-legend-child">
-          <div class="legend_item" id="legend-wu-ir" >
-            <label class="legend_wrap">
-            <input type="legend" name="legend" class="legend-inp">
-            <span class="legend_mark"></span>
-              Crop irrigation
-            </label>
-          </div>
-          <div class="legend_item" id="legend-wu-ps" >
-            <label class="legend_wrap">
-            <input type="legend" name="legend" class="legend-inp">
-            <span class="legend_mark"></span>
-              Public supply
-            </label>
-          </div>
-          <div class="legend_item" id="legend-wu-te-fresh" >
-            <label class="legend_wrap">
-            <input type="legend" name="legend" class="legend-inp">
-            <span class="legend_mark"></span>
-              Thermoelectric (fresh)
-            </label>
-          </div>
-          <div class="legend_item" id="legend-wu-te-saline" >
-            <label class="legend_wrap">
-            <input type="legend" name="legend" class="legend-inp">
-            <span class="legend_mark"></span>
-              Thermoelectric (saline)
-            </label>
-          </div>
+        <div class="caption-legend-child color-legend-single">
+          <ColorLegend legend-id="legend-wu-ir" label="Crop irrigation" color="var(--wu-agriculture)" />
+          <ColorLegend legend-id="legend-wu-ps" label="Public supply" color="var(--wu-ps)" />
+          <ColorLegend legend-id="legend-wu-te-fresh" label="Thermoelectric (fresh)" color="var(--wu-te-fresh)" />
+          <ColorLegend legend-id="legend-wu-te-saline" label="Thermoelectric (saline)" color="var(--wu-te-saline)" />
           </div>
           <div class="caption-text-child">
           <p>Bar chart showing modeled total daily water use averaged by year from 2010 through 2020 for the lower 48 United States. Toggle between viewing the total annual use versus the annual use by category. Note that the y-scale is not constant across use categories when viewing use by category.</p>
@@ -68,17 +44,20 @@
       <div class="text-container">
         <p>Other categories of water use such as mining, aquaculture, livestock, and domestic and industrial (from non-public supply sources), that together account for 10% of water use in the country, can also be locally or regionally important.</p>
       </div>
-
+      <br>
+      <br>
       <div class="text-container">
-        <h3>Not all the water withdrawn for human water use returns to the local environment</h3>
+        <h2>Not all the water withdrawn for human water use returns to the local environment</h2>
       </div>
       <div class="viz-container">
         <img class="viz-placeholder" src="https://labs.waterdata.usgs.gov/visualizations/images/water-availability/07_consumptive_labels.png"/>
       </div>
+      <br>
       <div class="text-container">
         <p>Water that does not return to local water bodies or groundwater is lost to the atmosphere, consumed by humans or livestock, or incorporated into products or crops. Across the lower 48 United States, the proportion of water use consumed in these ways is 72% for irrigation, 12% for public supply, and 4% for thermoelectric power. </p>
       </div>
-      
+      <br>
+      <br>
       <Methods></Methods>
       <References :theseReferences="referenceList"></References>
     </div>
@@ -99,6 +78,7 @@ import References from '../components/References.vue';
 import SubPages from '../components/SubPages';
 import { isMobile } from 'mobile-device-detect';
 import ToggleSwitch from '../components/ToggleSwitch.vue';
+import ColorLegend from '../components/ColorLegend.vue';
 
 // dependency injections
 const featureToggles = inject('featureToggles');
@@ -130,8 +110,8 @@ const barSpace = 5;
 
 // chart dimensions
 const margin = mobileView
-  ? { top: 10, right: 10, bottom: 50, left: 40 } //  mobile
-  : { top: 10, right: 10, bottom: 50, left: 40 }; // desktop
+  ? { top: 30, right: 10, bottom: 50, left: 40 } //  mobile
+  : { top: 30, right: 10, bottom: 50, left: 40 }; // desktop
 
 const width = containerWidth - margin.left - margin.right;
 const height = containerHeight - margin.top - margin.bottom;
@@ -204,7 +184,7 @@ async function loadDatasets() {
 function initBarChart() {
   // Remove any existing SVG to prevent duplicates
   d3.select('#barplot-container').select('svg').remove();
-  
+
   svg = d3.select('#barplot-container')
     .append('svg')
     .attr('class', 'barplotSVG')
