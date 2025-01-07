70de7ba8e3325352a84056de6c38be29d991db3a
Althea A Archer <aaarcher@usgs.gov>
2025-01-07 14:17:11 -0600

    refs kf3

--- src/views/keyFinding03SUISVI.vue
+++ src/views/keyFinding03SUISVI.vue
@@ -3,7 +3,7 @@
         <KeyMessages></KeyMessages>
         <div class="content-container">
             <div class="text-container">
-              <p>About 26.7 million people, or 8% of the population, live in areas of high or severe water limitation. The availability of clean water for our communities is affected by hydrologic factors like water quantity, quality, and flow, as well as social factors like water infrastructure, access, and rights.<span v-for="reference in theseReferences.filter(item => item.refID === 'Stets2025')" :key="reference.refID" class="tooltip"> <sup class="in-text-number">{{ reference.referenceNumber }} </sup> <span class="tooltiptext"> {{ reference.refID }}</span></span> Socioeconomic status, access to resources, and the availability of public services can determine a community's adaptive capacity. The adaptive capacity ultimately affects how vulnerable or resilient a community may be when faced with water-availability limitations.</p>
+              <p>About 26.7 million people, or 8% of the population, live in areas of high or severe water limitation.<span v-for="reference in theseReferences.filter(item => item.refID === 'Stets2025')" :key="reference.refID" class="tooltip"> <sup class="in-text-number">{{ reference.referenceNumber }} </sup> <span class="tooltiptext"> {{ reference.label }}</span></span> The availability of clean water for our communities is affected by hydrologic factors like water quantity, quality, and flow, as well as social factors like water infrastructure, access, and rights. Socioeconomic status, access to resources, and the availability of public services can determine a community's adaptive capacity. The adaptive capacity ultimately affects how vulnerable or resilient a community may be when faced with water-availability limitations.</p>
             </div>
 
             <div class="chart-title-container">
@@ -23,13 +23,13 @@
                 <ColorLegend legend-id="legend-sui-severe" label="Severe (0.8 - 1.0)" color="var(--sui-severe)" />
                         </div>
               <div class="caption-text-flex caption-text-child">
-                <p>Map showing circles for each hydrologic unit (HUC8). The color of the circle is the level of water limitation, and the size of the bubble represents the social vulnerability score, with larger bubbles representing more social vulnerability.<span v-for="reference in theseReferences.filter(item => item.refID === 'Census2020')" :key="reference.refID" class="tooltip"> <sup class="in-text-number">{{ reference.referenceNumber }} </sup> <span class="tooltiptext"> {{ reference.refID }}</span></span></p>
+                <p>Map showing circles for each hydrologic unit (HUC8). The color of the circle is the level of water limitation, and the size of the bubble represents the social vulnerability score, with larger bubbles representing more social vulnerability.<span v-for="reference in theseReferences.filter(item => item.refID === 'CDC2022')" :key="reference.refID" class="tooltip"> <sup class="in-text-number">{{ reference.referenceNumber }} </sup> <span class="tooltiptext"> {{ reference.refID }}</span></span></p>
               </div>
             </div> 
             <br>
             <div class="text-container">
                 <h2>Water insecurity</h2>
-                <p>Despite tremendous gains throughout the 20th century, limitations to water access and quality persist in the United States. These limitations disproportionately affect <a href="https://labs.waterdata.usgs.gov/visualizations/vulnerability-indicators/index.html#/en" target="_blank">socially vulnerable</a> communities. Societal factors shape exposure to hazards, susceptibility to suffer harm, and ability to cope and recover from losses (Drakes et al., 2024). </p>
+                <p>Despite tremendous gains throughout the 20th century, limitations to water access and quality persist in the United States. These limitations disproportionately affect <a href="https://labs.waterdata.usgs.gov/visualizations/vulnerability-indicators/index.html#/en" target="_blank">socially vulnerable</a> communities. Societal factors shape exposure to hazards, susceptibility to suffer harm, and ability to cope and recover from losses.<span v-for="reference in theseReferences.filter(item => item.refID === 'Drakes2024')" :key="reference.refID" class="tooltip"> <sup class="in-text-number">{{ reference.referenceNumber }} </sup> <span class="tooltiptext"> {{ reference.refID }}</span></span> </p>
             </div>
             <div class="chart-title-container">
               <p class="chart-title">Social vulnerability and water limitation</p>
@@ -56,13 +56,13 @@
             </div>
             <div class="caption-container">
               <div class="caption-text-child">
-                <p>Bar charts showing the proportion of the population of the lower 48 United States living in varying categories of social vulnerability and water limitation. The color is the level of water limitation.<span v-for="reference in theseReferences.filter(item => item.refID === 'Census2020')" :key="reference.refID" class="tooltip"> <sup class="in-text-number">{{ reference.referenceNumber }} </sup> <span class="tooltiptext"> {{ reference.refID }}</span></span></p>
+                <p>Bar charts showing the proportion of the population<span v-for="reference in theseReferences.filter(item => item.refID === 'Census2020')" :key="reference.refID" class="tooltip"> <sup class="in-text-number">{{ reference.referenceNumber }} </sup> <span class="tooltiptext"> {{ reference.refID }}</span></span> of the lower 48 United States living in varying categories of social vulnerability and water limitation. The color is the level of water limitation.</p>
               </div>
             </div> 
             <br>
             <div class="text-container">
                 <h2>Access to clean water</h2>
-                <p>Exposure to drinking-water contamination occurs at higher rates for low-income communities, minority-dominated communities, and those who depend on domestic wells as their drinking water source, compared to communities who are not socially vulnerable (Erickson2025xxx). While nitrogen and phosphorus pollution are problems for most communities in the U.S., watersheds with the highest levels of nitrogen and phosphorus contamination contain relatively higher proportions of socially vulnerable individuals. </p>
+                <p>Exposure to drinking-water contamination occurs at higher rates for low-income communities, minority-dominated communities, and those who depend on domestic wells as their drinking water source, compared to communities who are not socially vulnerable.<span v-for="reference in theseReferences.filter(item => item.refID === 'Erickson2025')" :key="reference.refID" class="tooltip"> <sup class="in-text-number">{{ reference.referenceNumber }} </sup> <span class="tooltiptext"> {{ reference.refID }}</span></span> While nitrogen and phosphorus pollution are problems for most communities in the U.S., watersheds with the highest levels of nitrogen and phosphorus contamination contain relatively higher proportions of socially vulnerable individuals. </p>
             </div>
             <br>
         <Methods></Methods>
