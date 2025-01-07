bff7054b8c2144872652ba56b585ba0ae914af2a
Althea A Archer <aaarcher@usgs.gov>
2025-01-07 14:16:54 -0600

    refs kf1

--- src/components/SubPages.js
+++ src/components/SubPages.js
@@ -15,19 +15,12 @@ export default {
             shortTitle: 'Water availability is affected by water quantity, quality, and flow',
             keyMessage: 'Every region in the lower 48 United States faces limitations to water availability from at least one aspect of water quantity, quality, or flow.',
             references: [
-                "VanMetre2020", 
                 "Stets2025", 
-                "Vörösmarty2010", 
-                "Lin2012", 
-                "Park2018", 
                 "Erickson2025", 
-                "Kaushal2018", 
-                "Waite2019", 
-                "Waite2021", 
-                "Kennedy2016", 
-                "Eng2019", 
-                "Moreira2019", 
-                "EPA2020"],
+                "VanMetre2020", 
+                "Scholl2025",
+                "IPCC2022",
+                "Crimmins2023"],
         },
         {
             page: 3,
@@ -36,15 +29,10 @@ export default {
             shortTitle: 'Socially vulnerable Americans have a higher risk of water limitation',
             keyMessage: 'Water limitation disproportionately affects socially vulnerable populations in the U.S., creating problems for equitable access to clean water.',
             references: [
-                "VanMetre2020",  
-                "Census2020", 
-                "Drakes2024", 
                 "Stets2025", 
-                "Meehan2020", 
-                "Mueller2021", 
-                "Scanlon2023", 
-                "Deitz2019", 
-                "Flanagan2011", 
+                "CDC2022",
+                "Drakes2024", 
+                "Census2020", 
                 "Erickson2025"]
         },
         {
@@ -54,13 +42,12 @@ export default {
             shortTitle: 'Limited water supply can cause water imbalance',
             keyMessage: 'Periods of low precipitation can have cascading effects throughout the water cycle, expanding areas of water limitation' ,
             references: [
-                "VanMetre2020", 
                 "Gorski2025",
-                "Stets2025",
-                "Medalie2025",
                 "Foks2025a",
                 "Foks2025b",
-                "Sampson2025"
+                "Sampson2025",
+                "VanMetre2020",
+                "Medalie2025"
             ]
         },
         {
@@ -70,16 +57,13 @@ export default {
             shortTitle: 'Natural and human sources of nutrients degrade water quality',
             keyMessage: 'Sources of nutrients in our water vary across the United States. Common sources of nutrients such as nitrogen and phosphorus include fertilizer and manure, atmospheric deposition, wastewater treatment plants, urban land, and a range of natural sources including stream channel and geologic sources.',
             references: [
-                "VanMetre2020", 
                 "Erickson2025", 
-                "Shoda2019", 
-                "Wurtsbaugh2019", 
+                "VanMetre2020", 
                 "Ator2019", 
                 "Hoos2019", 
                 "Robertson2019", 
                 "Wise2019a", 
-                "Wise2019b", 
-                "Martinez2024"]
+                "Wise2019b"]
         },
         {
             page: 6,
@@ -88,22 +72,12 @@ export default {
             shortTitle: 'Water quality issues can affect human and aquatic health',
             keyMessage: 'Drinking water, fish consumption, and recreational water use face different water quality threats.',
             references: [
-                "VanMetre2020", "Erickson2025",
+                "Erickson2025",
+                "Belitz2022", 
                 "EPA2023",
-                "Vareda2019",
-                "Canedo2013",
-                "Kaushal2018",
                 "Stets2020",
-                "Gillespie2018",
-                "AMAP2009",
-                "Lauber2017",
-                "Wentz2014",
                 "Ngoubeyou2022",
-                "Cabral2010",
-                "Verhougstraete2015",
-                "EPA2020",
-                "Belitz2022",
-                "Morris1995"
+                "Cabral2010"
             ]
         },
         {
@@ -112,7 +86,12 @@ export default {
             mainTitle: 'For farm, home, and energy',
             shortTitle: 'The largest demands for water are for crop irrigation, public supply, and thermoelectric power',
             keyMessage: 'About 224,000 million gallons of freshwater are used every day for irrigation (48%), public supply (16%), and thermoelectric power (36%).',
-            references: ["Medalie2025", "Tesoriero2013", "VanMetre2020", "Konar2020", "Erickson2025", "Dieter2018", "Symonds2015", "Eppehimer2021", "Rice2017", "Raptis2016", "Chini2020", "Galanter2023", "Luukkonen2023", "Martin2023", "Haynes2024"]
+            references: [
+                "Medalie2025", 
+                "Galanter2023", 
+                "Luukkonen2023", 
+                "Martin2023", 
+                "Haynes2024"]
         },
         {
             page: 8,
@@ -120,7 +99,13 @@ export default {
             mainTitle: 'From sea to shining sea',
             shortTitle: 'How we use water varies across the country',
             keyMessage: 'The Western and High Plains regions use water primarily for crop irrigation, while the eastern half of the country uses water primarily for public supply or thermoelectric power.',
-            references: ["VanMetre2020", "Medalie2025", "Galanter2023", "Luukkonen2023", "Martin2023", "Haynes2024"]
+            references: [
+                "Medalie2025", 
+                "VanMetre2020", 
+                "Galanter2023", 
+                "Luukkonen2023", 
+                "Martin2023", 
+                "Haynes2024"]
         }
     ]
 };
